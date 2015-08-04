59792a499d4d41cc6ce321b46da52892c5815f43
Mark Seemann <mark@ploeh.dk>
2015-08-04 18:03:52 +0200

    Implemented the hull function.
    This may seem like quite a coarse-grained commit, but the implementation
    is essentially a copy of the prototype that was used to paint the hulls
    that could then be visually inspected.
    
    The purpose of this test suite is actually to enable refactoring of the
    hull function.
    
    The main problem of the hull function is the procedural hullPoints
    function, which relies on mutation.

--- Geometry/Geometry.fs
+++ Geometry/Geometry.fs
@@ -8,4 +8,41 @@ let inline turn (x1, y1) (x2, y2) (x3, y3) =
     elif prod < LanguagePrimitives.GenericZero then Direction.Right
     else Direction.Straight
 
-let inline hull points = [(3., 1.); (3., 7.); (1., 6.); (1., 2.)]
+let inline hull points =
+    let inline compareLexigraphic (x1, y1) (x2, y2) = compare (y1, x1) (y2, x2)
+
+    let inline comparePolar p0 p1 p2 =
+        match turn p0 p1 p2 with
+        | Direction.Left -> -1
+        | Direction.Straight -> 0
+        | _ -> 1
+
+    let cmp p1 p2 =
+        let p0 = points |> List.sortWith compareLexigraphic |> List.head
+        let polarCmp = comparePolar p0 p1 p2
+        match polarCmp with
+        | 0 -> compareLexigraphic p1 p2
+        | _ -> polarCmp
+
+    let inline check points =
+        let rec checkImp = function
+            | [p1; p2; p3] when turn p1 p2 p3 = Direction.Right -> [p1; p3]
+            | [p1; p2; p3] -> [p1; p2; p3]
+            | p :: ps -> p :: checkImp ps
+            | [] -> []
+        let newPoints = checkImp points
+        newPoints.Length <> points.Length, newPoints
+
+    let inline hullPoints points =
+        let mutable ps = []
+        for p in points do
+            ps <- ps @ [p]
+            let mutable shouldCheck = true
+            while shouldCheck do
+                let wasDiscarded, newPoints = check ps
+                shouldCheck <- wasDiscarded
+                if wasDiscarded then ps <- newPoints
+        ps
+
+    let sortedPoints = points |> List.sortWith cmp |> Seq.distinct
+    sortedPoints |> hullPoints
