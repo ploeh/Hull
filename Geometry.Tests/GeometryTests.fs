40b8cfdbafe4f9f3ae8eb03f8d6c6fb825fa2a20
Mark Seemann <mark@ploeh.dk>
2015-08-04 15:35:54 +0200

    Implemented turn function.

--- Geometry.Tests/GeometryTests.fs
+++ Geometry.Tests/GeometryTests.fs
@@ -4,7 +4,12 @@ open Xunit
 open Swensen.Unquote
 
 [<Theory>]
-[<InlineData(0, 0, 1, 0, 1, 1, Direction.Left)>]
+[<InlineData( 0,  0,  1,  0,  1,  1, Direction.Left)>]
+[<InlineData( 0,  0, -1, -1,  1, -1, Direction.Left)>]
+[<InlineData( 1,  1,  2,  2,  3,  2, Direction.Right)>]
+[<InlineData(-2, -3,  2,  2, -3, -9, Direction.Right)>]
+[<InlineData( 1,  1,  2,  2,  3,  3, Direction.Straight)>]
+[<InlineData(-2,  0,  0,  1,  2,  2, Direction.Straight)>]
 let ``turn returns correct result``
     (x1 : float)
     (y1 : float)
--- Geometry/Geometry.fs
+++ Geometry/Geometry.fs
@@ -2,4 +2,8 @@
 
 type Direction = Left = -1 | Straight = 0 | Right = 1
 
-let inline turn (x1, y1) (x2, y2) (x3, y3) = Direction.Left
+let inline turn (x1, y1) (x2, y2) (x3, y3) =
+    let prod = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1)
+    if prod > LanguagePrimitives.GenericZero then Direction.Left
+    elif prod < LanguagePrimitives.GenericZero then Direction.Right
+    else Direction.Straight
