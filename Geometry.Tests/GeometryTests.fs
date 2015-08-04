11c1eed15f52cba2258ee5b39705aaf4394cfd1e
Mark Seemann <mark@ploeh.dk>
2015-08-04 15:28:35 +0200

    Added a turn function,
    although its return value is still hard-coded, due to lack of test cases.

---
+++ Geometry.Tests/GeometryTests.fs
@@ -0,0 +1,19 @@
+﻿module Ploeh.Geometry.Tests
+
+open Xunit
+open Swensen.Unquote
+
+[<Theory>]
+[<InlineData(0, 0, 1, 0, 1, 1, Direction.Left)>]
+let ``turn returns correct result``
+    (x1 : float)
+    (y1 : float)
+    (x2 : float)
+    (y2 : float)
+    (x3 : float)
+    (y3 : float)
+    (expected : Direction) =
+    
+    let actual : Direction = turn (x1, y1) (x2, y2) (x3, y3)
+    expected =! actual
+
---
+++ Geometry/Geometry.fs
@@ -0,0 +1,5 @@
+﻿module Ploeh.Geometry
+
+type Direction = Left = -1 | Straight = 0 | Right = 1
+
+let inline turn (x1, y1) (x2, y2) (x3, y3) = Direction.Left
